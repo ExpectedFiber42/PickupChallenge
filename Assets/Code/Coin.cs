3e17934adc62f2aef2ab5c3ba95b897f5d794399
DizzyShark351 <k.hulst@outlook.com>
2015-09-14 11:45:51 +0200

    Finished

--- Assets/Code/Coin.cs
+++ Assets/Code/Coin.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Coin : Pickup//inherit van pickup
 {
+    //private float m_score = 0;
+
     //override = je maakt een extensie van die functie
     public override void PlayerHit(Player _GO)
     {
+        _GO.m_score += 1;
+        //Debug.Log(m_score);
         base.PlayerHit(_GO);//roep de basis classe nog wel aan(dus de playerhit in pickup)
         //voeg hier code toe zo dat de speler zijn score omhoog gaat
     }
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -3,12 +3,32 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    private float m_score = 0;
+    public float m_score = 0;
+    public bool slowOne = false;
+    public bool slowTwo = false;
+    public float playerSpeed = 50;
 
     // Update is called once per frame
     void Update()
     {
         //beweeg de speler naar rechts
-        this.transform.position += Vector3.right * Time.deltaTime;
+        this.transform.position += Vector3.right / playerSpeed;
+
+        if (m_score == 2)
+        {
+            if (!slowOne)
+            {
+                slowOne = true;
+                Debug.Log("Slower Movement Initiated");
+                playerSpeed = 100;
+            }
+        }
+
+        if(m_score == 3)
+        { 
+
+                this.transform.Rotate(new Vector3(0,0,5));
+            
+        }
     }
 }
